d368c6f40510d42db25391ff7e1d30e4ae2f1b2c
castle_mac <castle_mac@castle-macui-MacBookPro.local>
2021-03-19 20:11:05 +0900

    Add: OrderDetail 생성 및 UI 작업중
    - UI 작업만 진행중
    - CSS 작업은 미미함

---
+++ frontend/src/service/OrderDetail/OrderDetail.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+        <div class="title"><span>주문상세조회</span></div>
+
+        <span class="info-title">주문정보</span>
+        <div class="info-box">
+                <div>
+                    <span>주문번호</span>
+                    <span>20210317000056000</span>
+                </div>
+                <div>
+                    <span>주문일자</span>
+                    <span>2021.03.17</span>
+                </div>
+                <div>
+                    <span>주문자</span>
+                    <span>장성준</span>
+                </div>
+                <div>
+                    <span>결제금액</span>
+                    <span>36,020 원</span>
+                </div>
+                <div>
+                    <span>적립예정포인트</span>
+                    <span>360 원</span>
+                </div>
+        </div>
+
+        <span class="order-title"></span>
+        <div class="order-box">
+            <span class="order-dub-title">일반배송</span>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
